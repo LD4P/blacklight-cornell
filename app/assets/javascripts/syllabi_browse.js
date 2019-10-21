a5ea5e473099ac3cd506b43a381e9e9185c57b50
John Skiles Skinner <jno.skinner@gmail.com>
2019-10-21 16:54:58 -0500

    Create .js file that will be used to query OSP

---
+++ app/assets/javascripts/syllabi_browse.js
@@ -0,0 +1,7 @@
+// This code displays book suggestions that come from Open Syllabus Project
+// Here is a method of including JS that I am currently using below:
+// https://brandonhilkert.com/blog/page-specific-javascript-in-rails/
+
+$(".catalog.catalog-show").ready(function() {
+  console.log( "ready!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" );
+});
