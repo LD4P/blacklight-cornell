2a7119c1a8f1c25cd3fed155110694e0e1752fbe
John Skiles Skinner <jno.skinner@gmail.com>
2019-10-30 12:43:35 -0400

    Clean up browseld controller

--- app/controllers/browseld_controller.rb
+++ app/controllers/browseld_controller.rb
@@ -3,14 +3,7 @@ class BrowseldController < ApplicationController
   include Blacklight::Catalog
   include BlacklightCornell::CornellCatalog
   before_action :heading
-  #attr_accessible :authq, :start, :order, :browse_type
-  @@browse_index_author = ENV['BROWSE_INDEX_AUTHOR'].nil? ? 'author' : ENV['BROWSE_INDEX_AUTHOR']
-  @@browse_index_subject = ENV['BROWSE_INDEX_SUBJECT'].nil? ? 'subject' : ENV['BROWSE_INDEX_SUBJECT']
-  @@browse_index_authortitle = ENV['BROWSE_INDEX_AUTHORTITLE'].nil? ? 'authortitle' : ENV['BROWSE_INDEX_AUTHORTITLE']
-  @@browse_index_callnumber = ENV['BROWSE_INDEX_CALLNUMBER'].nil? ? 'callnum' : ENV['BROWSE_INDEX_CALLNUMBER']
-  def heading
-   @heading='Browse'
-  end
+
   def subject
   	# Read the callhierarchy and call numbers json
   	filepath =Rails.public_path.join("data/callhierarchy.json")
@@ -24,4 +17,11 @@ class BrowseldController < ApplicationController
   def authors
   	#Query solr index and pass back information for timeline as json
   end
+
+  private
+
+  def heading
+    @heading='Browse'
+  end
+
 end
