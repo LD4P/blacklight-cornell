2b397640737fa62cae86f7548a08b5799d688e66
John Skiles Skinner <jno.skinner@gmail.com>
2020-01-06 16:57:50 -0500

    Print book titles on page

--- app/assets/javascripts/full_text_search.js
+++ app/assets/javascripts/full_text_search.js
@@ -5,9 +5,16 @@ var fullTextSearch = {
     var googleBooksUrl = "https://www.googleapis.com/books/v1/volumes?q=" + q;
     fetch(googleBooksUrl)
       .then(response => response.json())
-      .then(data => $("#full-text-results").append(JSON.stringify(data)))
+      .then(data => fullTextSearch.parseBooksData(data))
     ;
+  },
+
+  parseBooksData: function(data) {
+    data['items'].forEach(
+      element => $("#full-text-results").append(JSON.stringify(element['volumeInfo']['title']))
+    );
   }
+
 };
   
 Blacklight.onLoad(function() {
