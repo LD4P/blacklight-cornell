5cced2c5007a8f465611f7fc491a93b78cc294c7
John Skiles Skinner <jno.skinner@gmail.com>
2020-02-24 11:56:56 -0500

    Display HathiTrust book results

--- app/assets/javascripts/full_text_search.js
+++ app/assets/javascripts/full_text_search.js
@@ -1,7 +1,7 @@
 // If zero results are found in searching the catalog, this code runs the search
 // in Google Books full-text search API. Any results are looked up in the catalog.
 
-var fullTextSearch = {
+const fullTextSearch = {
 
   // Pass the user's query to full text search data sources
   queryFullText: function() {
@@ -29,70 +29,96 @@ var fullTextSearch = {
   parseHathiTrustData: function(hathiTrustResults) {
     const subjects = hathiTrustResults['subjects'].map(x => x['label']);
     fullTextSearch.addSubjectsToView(subjects);
+    for (const htResult of hathiTrustResults['results']) {
+
+      // Extract OCLC and ISBN ids, add field name prefixes, put into array
+      const bookIdStrings = [];
+      for (const keyVal of htResult['ids']) {
+        if ('OCLC' in keyVal) {
+          bookIdStrings.push('number:'+keyVal['OCLC']);
+        }
+        if ('ISBN' in keyVal) {
+          bookIdStrings.push('isbnissn:'+keyVal['ISBN']);
+        }
+      }
+      // Only search the catalog if there are one or more identifiers to search with
+      if (bookIdStrings.length > 0) {
+        // prepare Solr query string to search for a particular book
+        const bookQuery = bookIdStrings.join(' OR '); // boolean join the prefixed ids
+        // run the Solr query to check catalog for the book
+        fullTextSearch.checkSolr(bookQuery, htResult['title'], 'Result from HathiTrust')
+      }
+    }
   },
 
   parseGoogleBooksData: function(googleBooksResults) {
     // Iterate over each Goole Books result and see if it's in the catalog
     googleBooksResults['items'].forEach(function (gbResult) {
       // Extract 2 ISBN types, add field name prefixes, put them in array
-      var prefixedIsbns = gbResult['volumeInfo']['industryIdentifiers'].filter(function(i){
+      const prefixedIsbns = gbResult['volumeInfo']['industryIdentifiers'].filter(function(i){
         return ['ISBN_10', 'ISBN_13'].includes(i['type'])
-      }).map(x => 'isbnissn:' + x['identifier'])
+      }).map(x => 'isbnissn:' + x['identifier']);
       // Extract OCLC ids, add field name prefixes, put into array
-      var prefixedOclcs = gbResult['volumeInfo']['industryIdentifiers'].filter(function(j){
+      const prefixedOclcs = gbResult['volumeInfo']['industryIdentifiers'].filter(function(j){
         return ('OCLC' == j['identifier'].split(':')[0])
-      }).map(y => 'number:' + y['identifier'].split(':')[1])
+      }).map(y => 'number:' + y['identifier'].split(':')[1]);
       // Merge the arrays of prefixed ISBN and OCLC identifiers
-      var bookIdStrings = prefixedIsbns.concat(prefixedOclcs)      
+      const bookIdStrings = prefixedIsbns.concat(prefixedOclcs);  
       // Only search the catalog if there are one or more identifiers to search with
       if (bookIdStrings.length > 0) {
         // prepare Solr query string to search for a particular book
-        var solrAddrs = fullTextSearch.getSolrAddrs();
-        var BookQuery = bookIdStrings.join(' OR '); // boolean join the prefixed ids
-        var solrQuery = solrAddrs + "/select?wt=json&rows=1&q=" + BookQuery;
+        const bookQuery = bookIdStrings.join(' OR '); // boolean join the prefixed ids
         // run the Solr query to check catalog for the book
-        $.ajax({
-          url: solrQuery,
-          type: 'GET',
-          dataType: 'jsonp',
-          jsonp: 'json.wrf', // avoid CORS and CORB errors
-          complete: function(response) {
-            var numFound = response['responseJSON']['response']['numFound']
-            if (numFound > 0) { // if found, prepare a link to the view of that book
-              var title = response['responseJSON']['response']['docs'][0]['title_display']
-              var idNum = response['responseJSON']['response']['docs'][0]['id']
-              var cover = response['responseJSON']['response']['docs'][0]['oclc_id_display']
-               fullTextSearch.addBookToView(
-                '/catalog/' + idNum,
-                title, // Use the catalog title rather than Google Books title
-                gbResult['searchInfo']['textSnippet'],
-                cover
-              )
-            }
-          }
-        });
+        fullTextSearch.checkSolr(bookQuery, gbResult['searchInfo']['textSnippet'], 'Excerpt from Google Books')
       }
     })
     // Fill in book cover images
-    setTimeout(function(){ window.bookcovers.onLoad() }, 1000);
+    setTimeout(function(){ window.bookcovers.onLoad() }, 2000);
+  },
+
+  checkSolr: function(bookQuery, textSnippet, from) {
+    const solrAddrs = fullTextSearch.getSolrAddrs();
+    const solrQuery = solrAddrs + "/select?wt=json&rows=1&q=" + bookQuery;
+    $.ajax({
+      url: solrQuery,
+      type: 'GET',
+      dataType: 'jsonp',
+      jsonp: 'json.wrf', // avoid CORS and CORB errors
+      complete: function(response) {
+        const numFound = response['responseJSON']['response']['numFound']
+        if (numFound > 0) { // if found, prepare a link to the view of that book
+          const title = response['responseJSON']['response']['docs'][0]['title_display']
+          const idNum = response['responseJSON']['response']['docs'][0]['id']
+          const cover = response['responseJSON']['response']['docs'][0]['oclc_id_display']
+           fullTextSearch.addBookToView(
+            '/catalog/' + idNum,
+            title, // Use the catalog title rather than Google Books title
+            textSnippet,
+            from,
+            cover
+          )
+        }
+      }
+    });
   },
 
   // Add the new search results to the page
-  addBookToView: function(href, title, excerpt, cover) {
-    $("#full-text-results").append(
+  addBookToView: function(href, title, excerpt, from, cover) {
+    $("#full-text-results").prepend(
       '<div style="clear: left;">' +
       '<div style="float: left; padding: 0 15px 10px 0;"><img class="bookcover" id="OCLC:'+ cover +'" data-oclc="'+ cover +'" /></div>' +
       '<p><a href="' + href + '">' + title + '</a>' +
         '<br>' + excerpt +
-        '<br><span style="font-size: small;">Excerpt from Google Books</span>' +
+        '<br><span style="font-size: small;">'+ from +'</span>' +
       '</p></div>'
       
     );
   },
 
+  // Add subject links to the page from an array of strings
   addSubjectsToView: function(subjects) {
     $("#sidebar").append(
-      '<h3>Sujects</h3>' +
+      '<h3>Subjects</h3>' +
       '<ul style="list-style: none; padding: 0;">' +
         subjects.map(s => '<li><a href="/?f[fast_topic_facet][]='+s+'">'+s+'</a></li>').join('') +
       '<ul>'
