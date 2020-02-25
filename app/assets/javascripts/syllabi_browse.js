f5eeb3ad60c23a06166994c622dec9b388915a10
John Skiles Skinner <jno.skinner@gmail.com>
2020-02-25 10:24:18 -0500

    Osp async await (#22)
    * Extend async/await pattern to showing coursework slices
    
    * Move threashold (non-zero) check into its own function
    
    * Multi-line strings
    
    * Reorder functions and add comments
    
    * Move isbn boolean join into shared function
    
    * Formatting
    
    * Handle errors with try/catch
    
    * Comments

--- app/assets/javascripts/syllabi_browse.js
+++ app/assets/javascripts/syllabi_browse.js
@@ -1,111 +1,137 @@
-// This code displays book suggestions that come from Open Syllabus Project
+// Book recommendation engine using Open Syllabus Project (opensyllabus.org)
+// Created as a part of Linked Data for Production (ld4p.org)
 
-const getOpenSyllabusRecommendations = {
+const openSyllabus = {
 
-  // get co-assigned work suggestions & add to item view for a work
+  // Display books related to (coassigned with) the current book view
   getCoassignedBooks: async function(suggestions) {
-    const isbns = $( "#isbns-json-data" ).html();
-    const isbnParsed = JSON.parse(isbns);
-    const isbnParams = isbnParsed.join(',');
-    const coAssigned = await getOpenSyllabusRecommendations.queryOspCoassignmentsApi(isbnParams);
+    const isbns = $("#isbns-json-data").html();
+    const isbnsParam = JSON.parse(isbns).join(",");
+    const coAssigned = await this.queryOspCoassignmentsApi(isbnsParam);
+    if (!coAssigned) {
+      return; // stop execution if API call does not work
+    }
     for (const assignment of coAssigned) {
-      const joinIsbn = assignment.join(' OR ');
-      const queryCat = await getOpenSyllabusRecommendations.querySolrCheckSuggestion(joinIsbn);
-      const numFound = queryCat["response"]["numFound"];
-      if (numFound > 0) {
-        const result = queryCat["response"]["docs"][0]
-        getOpenSyllabusRecommendations.formatAndListSuggestions(result, joinIsbn); // write HTML
+      const queryCatalog = await this.querySolrCheckSuggestion(assignment);
+      if (this.hasSolrResults(queryCatalog)) {
+        const result = queryCatalog["response"]["docs"][0];
+        openSyllabus.formatAndListSuggestions(result, assignment); // write HTML
       }
     }
-    setTimeout(function(){ window.bookcovers.onLoad() }, 300); // fill cover images
+    window.bookcovers.onLoad(); // fill cover images
   },
 
-  // get OSP coassigments via pass-through internal API
-  queryOspCoassignmentsApi: function(isbnParams) {
-    const localRoute = '/browseld/osp_coassignments?isbns=';
-    return $.get(localRoute + isbnParams);
+  // Get coassignmenst via internal API that fronts OSP API
+  queryOspCoassignmentsApi: async function(isbnsParam) {
+    try {
+      const localRoute = "/browseld/osp_coassignments?isbns=";
+      return await $.get(localRoute + isbnsParam);
+    } catch (err) {
+      return false;
+    }
   },
 
-  // check OSP coassigment suggestions against Solr catalog
-  querySolrCheckSuggestion: function(joinIsbn) {
-    const solrServer = $( "#solr-server-url-data" ).html();
-    const solrParams = "/select?&wt=json&rows=1&q=" + joinIsbn;
-    return $.ajax({
-      url: solrServer + solrParams,
-      type: 'GET',
-      dataType: 'jsonp',
-      jsonp: 'json.wrf'
-    });
-  },
-
-  formatAndListSuggestions: function(result, joinIsbn) {
+  // Add a book recommendation to the page
+  formatAndListSuggestions: function(result, isbns) {
     // Display the div if something is found
     $(".browse-syllabi").show(500);
-    // Format author string
+    // Format author string if there is an author
+    let authorNote = '';
     if (result["author_display"]) {
-      var authorStringResponse = result["author_display"];
-      var authorDividedByComma = authorStringResponse.split(",");
-      var authorFirst2Elements = authorDividedByComma.slice(0,2).join();
+      const authorStringResponse = result["author_display"].split(",");
+      const authorFirst2Elements = authorStringResponse.slice(0, 2).join();
+      authorNote = ` by ${authorFirst2Elements}`;
     }
-    const authorNote = (authorFirst2Elements ? ' by '+authorFirst2Elements : '');
     // Set strings for title, href, and OCLC id
     const recomTitle = result["title_display"];
-    const recomQuery = '/catalog?&q='+joinIsbn;
-    const oclcIdDisp = result["oclc_id_display"][0]
+    const recomQuery = "/catalog?&q=" + isbns.join(" OR ");
+    const oclcIdDisp = result["oclc_id_display"][0];
     // Compose the HTML that will be displayed on the page
-    const htmlString = '<figure><div class="imgframe"><a href="'+recomQuery+'"><img class="bookcover" id="OCLC:'+oclcIdDisp+'" data-oclc="'+oclcIdDisp+'" /></a></div><figcaption><a href="'+recomQuery+'">'+recomTitle+'</a> '+authorNote+'</figcaption></figure>'
+    const htmlString = `
+      <figure>
+        <div class="imgframe">
+          <a href="${recomQuery}">
+            <img
+              alt="${recomTitle}"
+              class="bookcover"
+              id="OCLC:${oclcIdDisp}"
+              data-oclc="${oclcIdDisp}"
+            />
+          </a>
+        </div>
+        <figcaption>
+          <a href="${recomQuery}">${recomTitle}</a>${authorNote}
+        </figcaption>
+      </figure>
+    `;
     $("#recommended-list").append(htmlString);
   },
 
-  // This function checks the catalog, in slices or pages, for works listed in an academic field
-  checkFieldBooks: function(sliceSize, sliceNum) {
+  // Check books in a hidden table created by browseld/in_field.
+  // Each book found in catalog is shown, until the end of a slice,
+  // finishing with a "More" link to recurse for the next slice.
+  showFoundBooksSlice: function(sliceSize, sliceNum) {
     // Prepare "More..." link at bottom of table
-    var footerMoreBox = $('#footerMoreBox');
-    // Get Solr URL from env var via the DOM
-    var solrServerUrl = $( "#solr-server-url-data" ).html();
-    $('#appendedMore').hide("slow", function(){ $(this).remove(); })
+    const footerMoreBox = $("#footerMoreBox");
+    $("#appendedMore").hide("slow", function() {
+      $(this).remove();
+    });
     // Iterate on current slice of list of books
-    var fieldBookList = $('#fieldBookList tr');
-    var bookListSlice = fieldBookList.slice(sliceNum-sliceSize, sliceNum);
-    bookListSlice.each(function(){
-      var row = $(this)
-      row.find('.isbns').each(function() {
-        var isbns = JSON.parse($(this).text());
-        var joinedIsbns = isbns.join(' OR ');
-        var solrUrl = solrServerUrl + "/select?&wt=json&rows=0&q=" + joinedIsbns;
-        $.ajax({
-          url: solrUrl,
-          type: 'GET',
-          dataType: 'jsonp',
-          jsonp: 'json.wrf', // avoid CORS and CORB errors
-          complete: function(solrResponse) {
-            var numFound = solrResponse["responseJSON"]["response"]["numFound"]
-            if (numFound > 0) {
-              row.show();
-            }
-            // On last book in this slice, show "More..." link to run next slice
-            if (row[0] === bookListSlice.last()[0]) {
-              var nextSliceNum = sliceNum + sliceSize;
-              footerMoreBox.append(
-                '<a id="appendedMore" href="javascript:getOpenSyllabusRecommendations.checkFieldBooks(20,'
-                +nextSliceNum+
-                ')">More...</a>');
-            }
-          }
-        });
+    const fieldBookList = $("#fieldBookList tr");
+    const bookListSlice = fieldBookList.slice(sliceNum - sliceSize, sliceNum);
+    bookListSlice.each(function() {
+      const row = $(this);
+      row.find(".isbns").each(async function() {
+        const isbns = JSON.parse($(this).text());
+        const queryCatalog = await openSyllabus.querySolrCheckSuggestion(isbns);
+        if (openSyllabus.hasSolrResults(queryCatalog)) {
+          row.show(); // Show tr if it contains a book found in Solr catalog
+        }
+        // On last book in this slice, show "More..." link to run next slice
+        if (row[0] === bookListSlice.last()[0]) {
+          const nextSliceNum = sliceNum + sliceSize;
+          footerMoreBox.append(`
+            <button
+              id="appendedMore"
+              onClick="openSyllabus.showFoundBooksSlice(20,${nextSliceNum})"
+            >More...</button>
+          `);
+        }
       });
     });
+  },
+
+  // isbns: an array of strings designating a book to be looked up in Solr
+  querySolrCheckSuggestion: async function(isbns) {
+    const joinedIsbns = isbns.join(" OR ");
+    const solrServer = $("#solr-server-url-data").html();
+    const solrParams = "/select?&wt=json&rows=1&q=" + joinedIsbns;
+    // Using JSONP to avoid CORS errors, but it prevents use of try/catch
+    // https://forum.jquery.com/topic/jquery-ajax-with-datatype-jsonp-will-not-use-error-callback-if-request-fails
+    // TODO: handle errors here
+    return await $.ajax({
+      url: solrServer + solrParams,
+      type: "GET",
+      dataType: "jsonp",
+      jsonp: "json.wrf"
+    });
+  },
+
+  // solrResults: result of querySolrCheckSuggestion
+  hasSolrResults: function(solrResults) {
+    const numFound = solrResults["response"]["numFound"];
+    return numFound > 0;
   }
 
 };
 
 Blacklight.onLoad(function() {
   // Run syllabus coassignment code in item view
-  $('body.catalog-show, body.blacklight-catalog-show').each(function() {
-    getOpenSyllabusRecommendations.getCoassignedBooks();
+  $("body.catalog-show, body.blacklight-catalog-show").each(function() {
+    openSyllabus.getCoassignedBooks();
   });
   // Run books in field code in syllabus browse
-  $('body.browseld-in_field').each(function() {
-    getOpenSyllabusRecommendations.checkFieldBooks(20,20);
+  $("body.browseld-in_field").each(function() {
+    openSyllabus.showFoundBooksSlice(20, 20);
   });
 });
