985e2116a8685b21da7effb560988a9a71503e1f
tworrall <tlw72@cornell.edu>
2020-06-09 17:14:47 -0400

    updates to support index changes

--- app/assets/javascripts/autosuggest.js
+++ app/assets/javascripts/autosuggest.js
@@ -1,4 +1,3 @@
-// Add autocomplete for the main site search box — ony for Best Bets results
 (function ($) {
   $(document).ready(function() {
     if ($('#q').length) {
@@ -9,57 +8,29 @@
               type: 'GET',
               dataType: "json",
               complete: function(data) {
-                  x = JSON.parse(data["responseText"]);
-                  items = [];
-                  $.each(x, function(i,v) {
-                      items.push(i + "s");
-                      $.each(v, function() {
-                        tmpHash = {}
-                        pseudoHash = {}
-                        tmpHash["label"] = this["label_s"] + " <span>(" + this["rank_i"] + ")</span>";
-                        tmpHash["type"] = this["type_s"];
-                        tmpHash["value"] = this["label_s"];
-                        if ( this["variant"] != undefined ) {
-                            tmpHash["label"] += "<li style='margin-left:15px;'><em>aka:</em> " + this["variant"] + "</li>";
-                        }
-                        items.push(tmpHash);
-
-                        // pseudonyms have to go into the hash as separate items, and after the preferred label,
-                        // because they can be searched individually, unlike variants
-                        if ( this["pseudonym"] != undefined && this["pseudonym"].length > 0 ) {
-                            console.log(this["pseudonym"].toSource());
-                            var the_type = this["type_s"];
-                            $.each(this["pseudonym"], function() {
-                                pseudoHash["label"] = "<div style='margin:-6px 0 0 15px;'><em>See also:</em> " + this + "</div>";
-                                pseudoHash["type"] = the_type;
-                                pseudoHash["value"] = this;
-                                items.push(pseudoHash);
-                            });
-                        }
-                      });
-                  });
+                  items = JSON.parse(data["responseText"]);
                   response( items );
               }
             });
          },
          minLength: 3,
          select: function(event, ui) {
-          if ( ui.item.type == "author" ) {
-              $('#search_field').val('author/creator');
-          }
-          else {
-              $('#search_field').val('subject');
-          }
-          if ( ui.item.label.indexOf("<span>") > -1 ) {
-            $('#q').val(ui.item.label.substring(0,ui.item.label.indexOf(" <span>")));
-          }
-          if ( ui.item.label.indexOf("See also") > -1 ) {
-            tmp = ui.item.label.replace("<em>See also:/<em>","");
-            $('#q').val(ui.item.label.substring(ui.item.label.indexOf("</em>")+5,ui.item.label.indexOf(" <span>")));
-          }
-          $('form#search-form').submit();
-          return false;
-        }
+             if ( ui.item.type == "author" ) {
+                 $('#search_field').val('author/creator');
+             }
+             else {
+                 $('#search_field').val('subject');
+             }
+             if ( ui.item.label.indexOf("<span>") > -1 ) {
+               $('#q').val(ui.item.label.substring(0,ui.item.label.indexOf(" <span>")));
+             }
+             if ( ui.item.label.indexOf("See also") > -1 ) {
+               tmp = ui.item.label.replace("<em>See also:/<em>","");
+               $('#q').val(ui.item.label.substring(ui.item.label.indexOf("</em>")+5,ui.item.label.indexOf(" <span>")));
+             }
+             $('form#search-form').submit();
+             return false;
+           }
       })
       .autocomplete( "instance" )._renderItem = function( ul, item ) {
           if ( item.label == "Authors" || item.label == "Locations" || item.label == "Subjects" || item.label == "Genres" ) {
@@ -71,14 +42,7 @@
               $x.html(item.label);
           }
           return $x.appendTo(ul);
-        };
+      };
     }
   });
 })(jQuery);
-
-// When the IE9 warning message is dismissed, send an AJAX call to the server 
-// to remember that fact in the user session so that he/she doesn't keep seeing
-// the same message
-function hideIE9Warning() {
-    $.post('/backend/dismiss_ie9_warning');
-}
--- app/controllers/autosuggest_controller.rb
+++ app/controllers/autosuggest_controller.rb
@@ -5,83 +5,104 @@ class AutosuggestController < CatalogController
   def get_suggestions
     # eliminate commas from the search term; e.g. "einstein, albert"
     query = params["term"].gsub(",","")
-    solr_url = ENV["AUTOSUGGEST_URL"] + "/suggest/select?hl=on&indent=on&wt=json&sort=rank_i+desc&rows=50&q=" + query.to_s;
-    url = URI.parse(solr_url)
-    resp = Net::HTTP.get_response(url)
-    data = resp.body
-    result = JSON.parse(data)
+    solr_url = ENV["AUTOSUGGEST_URL"] + "/suggest/select?hl=on&indent=on&wt=json&sort=rank_i+desc&rows=100&q=" + query.to_s;
+    result = get_solr_response(solr_url)
     docs = result["response"]["docs"]
     # Will need the highlighting later for variants
     highlights = result["highlighting"]
     # group the solr docs by type: author, subject, etc.
-    grouped = result["response"]["docs"].group_by { |h| h["type_s"]}
+    grouped = docs.group_by { |h| h["type_s"]}
     grouped = grouped.sort.to_h
     # compile an array of the preferred labels
-    labels = docs.map{|n| n["label_s"]}
+    pseudos = [""]
+    tmp_hash = {}
+    docs.each do |d|
+      tmp_hash = eval(d["pseudonyms_ss"][0])  if d["pseudonyms_ss"].present?
+      pseudos << tmp_hash[:label]  if !tmp_hash.empty?
+      tmp_hash = {}
+    end
     my_hash = {}
     # now loop through each group and sort by rank
     grouped.each do |k, v|
       x = v.sort_by { |k| -k["rank_i"] }
       autosuggest_terms = []
-      # special processing for variants and pseudonymns
-      x.each do |i|
-        unless autosuggest_terms.join(",").downcase.include?(i["label_s"].downcase) || 
-          autosuggest_terms.join(",").downcase.include?(i["label_s"].gsub(/\.$/, '').downcase)
-          # if the query term doesn't match a preferred label, we have a variant or pseudonym.
-          if !term_matches_label(query.downcase,i["label_s"])
-            # Whether it's a variant or pseudonym, use the id to grab the suggested label from the highlighting hash.
-            tmp_string = highlights[i["id"]]["label_suggest"][0].gsub("<em>","").gsub("</em>","")
-            # ensure the query term matches a variant and not a pseudonym
-            if !i["pseudonyms_t"].present?
-              i["variant"] = tmp_string unless tmp_string == i["label_s"]
-              autosuggest_terms << i
-            # query term doesn't match a variant, but does it match a pseudonym
-            elsif term_matches_label(query.downcase,i["pseudonyms_t"].join(","))
-              # We only want pseudonyms that are not catalog entries (they won't be in the preferred labels array).
-              if !labels.join(",").downcase.include?(tmp_string.downcase)
-                # pseudonyms with no catalog entries get marked as variants (the "aka" display)
-                i["variant"] = tmp_string unless tmp_string == i["label_s"]
-                
-                # there could be other pseudonyms, some with catalog entries. loop through them and check
-                # if any have entries, add them to the hash. The has_catalogue_pseudonym method returns 0 when
-                # the pseudonym does not have entries, and it returns the rank_i value when there are entries.
-                i["pseudonym"] = []
-                i["pseudonyms_ss"].each do |p|
-                  rank = has_catalogued_pseudonym(eval(p)[:uri])
-                  if rank > 0
-                    i["pseudonym"] << eval(p)[:label] + " <span>(" + rank.to_s + ")</span>"
-                  end
-                end
-                autosuggest_terms << i
-              end
-            end
-          else
-            # If this solr doc has any pseudonymns and they're in the preferred labels array,
-            # add them here. They'll appear as "see also" items in the UI. 
-            # 
-            if i["pseudonyms_t"].present?
-              i["pseudonym"] = []
-              i["pseudonyms_t"].each do |p|
-                if labels.join(",").downcase.include?(p.downcase)
-                  rank = docs.select{|n| n["label_s"] == p}.map{|r| r["rank_i"]}
-                  # workaround for inconsistent use of periods at the end of preferred labels
-                  if rank.empty?
-                    rank = docs.select{|n| n["label_s"].gsub(/\.$/, '') == p}.map{|r| r["rank_i"]}
-                  end
-                  i["pseudonym"] << p + " <span>(" + rank[0].to_s + ")</span>"
-                end
-              end
-            end
-            autosuggest_terms << i
+      # Take the top 6 in each group.
+      x.take(6).each do |i|
+        add_term = true
+        # Special processing for variants and pseudonymns. If the query term doesn't match a preferred 
+        # label, we have a variant or pseudonym.
+        if !term_matches_label(query.downcase,i["label_s"])
+          # For rare cases where query term matches the variant of a preferred label where that preferred label
+          # is a pseudonym_ss for a match on the query term. E.g., term = "smith" and "smith" is part of a variant
+          # of Joyce Carol Oates, while JCO is a pseudonym_ss for Rosamnd Smith.
+          if pseudos.join(", ").downcase.include?(i["label_s"].downcase)
+            add_term = false
+          end
+          # Whether it's a variant or pseudonym, use the id to grab the suggested label from the highlighting hash.
+          tmp_string = highlights[i["id"]]["label_suggest"][0].gsub("<em>","").gsub("</em>","")
+          # ensure the query term matches a variant and not a pseudonym
+          if !i["pseudonyms_t"].present?
+            i["variant"] = tmp_string unless tmp_string == i["label_s"]
+          # query term doesn't match a variant, but does it match a pseudonym
+          elsif term_matches_label(query.downcase,i["pseudonyms_t"].join(","))
+            # pseudonyms with no catalog entries get marked as variants (the "aka" display)
+            i["variant"] = tmp_string unless tmp_string == i["label_s"]
+          end
+        end
+        # If this solr doc has any pseudonymns_ss labels, add them here. 
+        # They'll appear as "see also" items in the UI. 
+        if i["pseudonyms_ss"].present?
+          i["pseudonym"] = []
+          i["pseudonyms_ss"].each do |p|
+            p_h = eval(p)
+            i["pseudonym"] << p_h[:label] + " <span>(" + p_h[:rank].to_s + ")</span>"
+          end
+        end
+        if i["wd_description_s"].present?
+          punc = " " if i["label_s"][-1] == "."
+          punc = ". " if i["label_s"][-1] != "."
+          i["label_s"] = i["label_s"] + punc + " <span>" + i["wd_description_s"] + "</span>"
+        end
+        autosuggest_terms << i unless add_term == false
+      end
+      my_hash[k.capitalize] = autosuggest_terms
+    end
+    items = prep_autosuggest_items(my_hash)
+    respond_to do |format|
+      format.json { render json: items }
+    end
+  end
+  
+  def prep_autosuggest_items(my_hash)
+    items = [];
+    my_hash.each do |k, v|
+      items << k + "s"
+      v.each do |i|
+        tmpHash = {}
+        pseudoHash = {}
+        tmpHash["label"] = i["label_s"] + " <span>(" + i["rank_i"].to_s + ")</span>";
+        tmpHash["type"] = i["type_s"];
+        tmpHash["value"] = i["label_s"];
+        if i["variant"].present? 
+          tmpHash["label"] += "<li style='margin-left:15px;'><em>aka:</em> " + i["variant"] + "</li>";
+        end
+        items << (tmpHash);
+
+        # pseudonyms have to go into the hash as separate items, and after the preferred label,
+        # because they can be searched individually, unlike variants
+        if i["pseudonym"].present? && i["pseudonym"].length > 0
+          the_type = i["type_s"];
+          i["pseudonym"].each do |l|
+            pseudoHash["label"] = "<div style='margin:-6px 0 0 15px;'><em>see also:</em> " + l + "</div>";
+            pseudoHash["type"] = the_type;
+            pseudoHash["value"] = l;
+            items << pseudoHash
+            pseudoHash = {}
           end
         end
       end
-      # take the top 6 in each group
-      my_hash[k.capitalize] = autosuggest_terms[0...6]
-    end    
-    respond_to do |format|
-      format.json { render json: my_hash }
     end
+    return items
   end
   
   def term_matches_label(term,label_s)
@@ -96,45 +117,15 @@ class AutosuggestController < CatalogController
         return label_s.match?(/\b#{query_array[0]}\b.*#{query_array[1]}.*#{query_array[2]}.*#{query_array[3]}|\b#{query_array[1]}\b.*#{query_array[0]}.*#{query_array[2]}.*#{query_array[3]}|\b#{query_array[2]}\b.*#{query_array[0]}.*#{query_array[1]}.*#{query_array[3]}|\b#{query_array[3]}\b.*#{query_array[0]}.*#{query_array[1]}.*#{query_array[2]}/i)
       end
     else
-      return label_s.match?(/\b#{query}\b/i)
+      return label_s.match?(/\b#{query}.*\b/i)
     end
   end
 
-  def has_catalogued_pseudonym(uri)    
-    solr_url = ENV["AUTOSUGGEST_URL"] + '/suggest/select?q=*:*&wt=json&fq=uri_s:"' + uri + '"';
+  def get_solr_response(solr_url)
     url = URI.parse(solr_url)
     resp = Net::HTTP.get_response(url)
     data = resp.body
     result = JSON.parse(data)
-    if result["response"]["numFound"] == 0
-      return 0
-    else
-      return result["response"]["docs"][0]['rank_i']
-    end
-  end
-
-  def original_get_suggestions
-    query = params["term"].gsub(",","")
-    if  query.strip.include? " "
-      query_array = query.split
-      if query_array.size == 2
-        x = SUGGESTIONS.select{|x| x["label"].match(/\b#{query_array[0]}\b.*#{query_array[1]}|\b#{query_array[1]}\b.*#{query_array[0]}/i)}
-      elsif query_array.size == 3
-        x = SUGGESTIONS.select{|x| x["label"].match(/\b#{query_array[0]}\b.*#{query_array[1]}.*#{query_array[2]}|\b#{query_array[1]}\b.*#{query_array[0]}.*#{query_array[2]}|\b#{query_array[2]}\b.*#{query_array[0]}.*#{query_array[1]}/i)}
-      elsif query_array.size == 4
-        x = SUGGESTIONS.select{|x| x["label"].match(/\b#{query_array[0]}\b.*#{query_array[1]}.*#{query_array[2]}.*#{query_array[3]}|\b#{query_array[1]}\b.*#{query_array[0]}.*#{query_array[2]}.*#{query_array[3]}|\b#{query_array[2]}\b.*#{query_array[0]}.*#{query_array[1]}.*#{query_array[3]}|\b#{query_array[3]}\b.*#{query_array[0]}.*#{query_array[1]}.*#{query_array[2]}/i)}
-      end
-    else
-      x = SUGGESTIONS.select{|x| x["label"].match?(/\b#{query}\b/i)}
-    end
-    grouped = x.group_by { |h| h["type"]}
-    my_hash = {}
-    grouped.each do |k, v|
-      x = v.sort_by { |k| -k["rank"] }
-      my_hash[k.capitalize] = x[0...6]
-    end
-    respond_to do |format|
-      format.json { render json: my_hash }
-    end
+    return result
   end
 end 
