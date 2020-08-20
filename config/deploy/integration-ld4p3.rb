50916b8aaeaf2b5f5e6c1415ce322a6d56b6465b
Greg Delisle <gad22@cornell.edu>
2020-08-20 14:10:18 -0400

    deploy second server

--- config/deploy/integration-ld4p3.rb
+++ config/deploy/integration-ld4p3.rb
@@ -1,5 +1,6 @@
 server 'aws-108-114.internal.library.cornell.edu',  :app, :web, :db, user: 'jenkins' 
 server 'aws-108-199.internal.library.cornell.edu',  :app, :web, :db, user: 'jenkins' 
+# deploy second server in cluster
 
 #server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
 #role :app, "aws-108-114.internal.library.cornell.edu"
